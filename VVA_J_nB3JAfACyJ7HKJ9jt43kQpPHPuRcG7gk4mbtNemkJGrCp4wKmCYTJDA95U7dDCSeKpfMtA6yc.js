493349dd458d26c24a852b9f719f38539e3d38d4
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-03-05 22:34:56 +0900

    Update VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js

--- VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js
+++ VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js
@@ -3,15 +3,11 @@ let fgnc = url.substr( 60 );;
 if (fgnc == "Function_Setting") {
    $('#Main_Title').html('Setting');
    $('#Main_Menu').html(' ');
-} else if (fgnc == "ツクツクボウシ" && fgnc == "つくつくぼうし") {
+} else if (fgnc == "ツクツクボウシ" || fgnc == "つくつくぼうし") {
    $('#Main_Title').html('Search');
 } else if (fgnc == '') {
 
 } else {
    $('#Main_Title').html('Search');
-   $('#Main_Menu').html('
-<div class="s_bar" align="center">
-<h4>検索結果に一致するデータが取得できませんでした</h4>
-</div>
-');
+   $('#Main_Menu').html('<div class="s_bar" align="center"><h4>検索結果に一致するデータが取得できませんでした</h4></div>');
 }
