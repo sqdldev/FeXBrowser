a79ce5391d057ffd7197e868224a912bc646f49c
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-03-05 19:39:36 +0900

    Update VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js

--- VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js
+++ VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js
@@ -5,6 +5,8 @@ if (fgnc == "Function_Setting") {
    $('#Main_Menu').html(' ');
 } else if (fgnc == "ツクツクボウシ" && fgnc == "つくつくぼうし") {
    $('#Main_Title').html('Search');
+} else if (fgnc == '') {
+
 } else {
    $('#Main_Title').html('Search');
    $('#Main_Menu').html('検索結果に一致するデータが取得できませんでした');
