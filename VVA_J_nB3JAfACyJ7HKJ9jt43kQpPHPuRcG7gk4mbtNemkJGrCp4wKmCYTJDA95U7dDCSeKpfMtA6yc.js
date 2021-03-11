e920e3e1ff70f74745b60026c5c75b55d12e8215
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-03-11 21:56:45 +0900

    Update VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js

--- VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js
+++ VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js
@@ -34,7 +34,7 @@ if (fgnc == "Function_Setting") {
    $('#Site_Name').html('FeX Browser - Setting');
    $('#Main_Title').html('Setting');
    $('#Main_Menu').html(' ');
-} else if (fgnc == "Function_History" && ) {
+} else if (fgnc == "Function_History" && fgnc !== "false") {
    $('#Site_Name').html('FeX Browser - History');
    $('#Main_Title').html('History');
    $('#Main_Menu').html('<div class="s_bar" align="center"><p align="left">　</p><h4><a href="https://figseu-technology.github.io/FeXBrowser/index.html?f=' + history_1 + '">　' + history_1 + '</a></h4></div><br> </br><div class="s_bar" align="center"><p align="left">　</p><h4><a href="https://figseu-technology.github.io/FeXBrowser/index.html?f=' + history_2 + '">　' + history_2 + '</a></h4></div><br> </br><div class="s_bar" align="center"><p align="left">　</p><h4><a href="https://figseu-technology.github.io/FeXBrowser/index.html?f=' + history_3 + '">　' + history_3 + '</a></h4></div><br> </br>');
