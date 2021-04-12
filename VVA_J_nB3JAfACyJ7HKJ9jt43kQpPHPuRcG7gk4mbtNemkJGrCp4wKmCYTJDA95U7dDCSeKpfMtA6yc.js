3db1088680a8383b66356ffa7a9729a845ed6f19
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-04-12 22:56:41 +0900

    Update VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js

--- VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js
+++ VVA_J_nB3JAfACyJ7HKJ9jt43kQpPHPuRcG7gk4mbtNemkJGrCp4wKmCYTJDA95U7dDCSeKpfMtA6yc.js
@@ -7,10 +7,12 @@ let fgnb = url.substr( 60, 1 );
 // データベース
 
 if (fgnc == "Function_Setting") {
+   // 基本動作
    $('#Site_Name').html('FeX Browser - Setting');
    $('#Main_Title').html('Setting');
    $('#Main_Menu').html(' ');
 } else if (fgnc == "%E3%81%82") {
+   // あ行
    $('#Site_Name').html('FeX Browser - Search');
    $('#Main_Title').html('Search');
    $('#Main_Menu').html('<div class="s_bar" align="center"><h3 align="left">　あ</h3><h5 align="left" class="Home_p">　平仮名</h5><hr size="3"/><h5>・五十音図ア行の第1音。五母音の一。後舌の開母音。</h5><h5>・平仮名「あ」は「安」の草体、片仮名「ア」は「阿」の偏から変化したもの。</h5><h5 align="left" class="Home_p">　国語辞書より</h5></div>');
@@ -27,10 +29,12 @@ if (fgnc == "Function_Setting") {
    $('#Main_Title').html('Search');
    $('#Main_Menu').html('<div class="s_bar" align="center"><h3 align="left">　ツクツクボウシ</h3><h5 align="left" class="Home_p">　昆虫</h5><img align="center" width="200px" src="img/28488525742287558213585485.jpg"><hr size="3"/><h5>細身で黒っぽく、緑色の斑紋があるセミ。姿よりも「オーシィ・ツクツク・オーシィ…」という鳴き声で有名。都市周辺でも普通に見られる。人の気配に敏感なので、どこにもいるわりには姿を見ることは少ない。夏の終わり頃に個体数が増えて鳴き声が目立つようになるので、秋を告げるセミとされる。</h5><h5 align="left" class="Home_p">　昆虫エクスプローラより</h5></div><div class="s_bar" align="center"><h4><a href="https://www.insects.jp/kon-semituku.htm">ツクツクボウシ - 昆虫エクスプローラ</a></h4><hr size="3"/><h5>ツクツクボウシ. カメムシ目 頸吻亜目 セミ科 セミ亜科. Meimuna opalifera. 大きさ （翅端まで）40-47mm; 時 期 7-10月; 分 布 北海道・本州・四国・九州. 細身で黒っぽく、</h5></div><br> </br><div class="s_bar" align="center"><h4><a href="https://ja.m.wikipedia.org/wiki/%E3%83%84%E3%82%AF%E3%83%84%E3%82%AF%E3%83%9C%E3%82%A6%E3%82%B7">ツクツクボウシ - Wikipedia</a></h4><hr size="3"/><h5>ツクツクボウシ（つくつく法師、寒蟬、Meimuna opalifera ） はカメムシ目（半翅目）・ヨコバイ亜目（同翅亜目）<h5><hr size="3"/><h5 align="left" class="Home_p">科： セミ科 Cicadidae</h5><h5 align="left" class="Home_p">目： カメムシ目（半翅目） Hemiptera<h5></div><br> </br><div class="s_bar" align="center"><h4><a href="https://m.youtube.com/results?sp=mAEA&search_query=ツクツクボウシ">ツクツクボウシ - YouTube</h4></a><hr size="3"/><h5>『ツクツクボウシ』の動画</h5></div>');
 } else if (fgnc == '') {
+   // ホーム
    $('#Site_Name').html('FeX Browser - Home');
    $('#Main_Title').html('APP');
    $('#Main_Menu').html('<div align="center"><p><a href="https://figseu-technology.github.io/RollingSkyEvolution/index.html"><img src="ic/Rolling Sky Evolution icon.png" width="25%"></a><a href="https://figseu-technology.github.io/FeXBrowser/index.html"><img src="ic/Empty.png" width="25%"></a><a href="https://figseu-technology.github.io/FeXBrowser/index.html"><img src="ic/Empty.png" width="25%"></a></p></div><div align="center"><p><a href="https://figseu-technology.github.io/FeXBrowser/index.html"><img src="ic/Empty.png" width="25%"></a><a href="https://figseu-technology.github.io/FeXBrowser/index.html"><img src="ic/Empty.png" width="25%"></a><a href="https://figseu-technology.github.io/FeX-Browser/index.html"><img src="ic/Empty.png" width="25%"></a></p></div>');
 } else {
+   // 404
    $('#Site_Name').html('FeX Browser - Search');
    $('#Main_Title').html('Search');
    $('#Main_Menu').html('<div class="s_bar" align="center"><h4>検索結果に一致するデータが取得できませんでした</h4></div>');
