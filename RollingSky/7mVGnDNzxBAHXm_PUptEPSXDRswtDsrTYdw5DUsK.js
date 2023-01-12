cd70ccde4844fcf0bb1967b40ecd2ced9f993a65
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2023-01-12 16:22:23 +0900

    Update 7mVGnDNzxBAHXm_PUptEPSXDRswtDsrTYdw5DUsK.js

--- RollingSky/7mVGnDNzxBAHXm_PUptEPSXDRswtDsrTYdw5DUsK.js
+++ RollingSky/7mVGnDNzxBAHXm_PUptEPSXDRswtDsrTYdw5DUsK.js
@@ -3,4 +3,18 @@ let fgnc = url.substr( 72 );
 
 if (fgnc == 1) {
    $('body').css({'background-image':'url(https://figseu-technology.github.io/FeXBrowser/RollingSky/RSBG/RSBG_2.png)'});
+   $('#level-name').html('<img src="STT/STT_1.png" width="96%">');
+   $('#level-view').html('<img src="RAA/RAA_1.png" width="92%">');
+} else if (fgnc == 443) {
+   $('body').css({'background-image':'url(https://figseu-technology.github.io/FeXBrowser/RollingSky/RSBG/RSBG_443.png)'});
+   $('#level-name').html('<img src="STT/STT_443.png" width="96%">');
+   $('#level-view').html('<img src="RAA/RAA_443.png" width="92%">');
+}
+
+function BGM() {
+   if (fgnc == 1) {
+	$('#st_1').get(0).play();
+   } else if (fgnc == 443) {
+	$('#st_443').get(0).play();
+   }
 }
