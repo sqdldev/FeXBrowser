9916c33673cb344f1fe2e54163a0b046ff269edf
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2023-01-12 21:34:05 +0900

    Update 7mVGnDNzxBAHXm_PUptEPSXDRswtDsrTYdw5DUsK.js

--- RollingSky/7mVGnDNzxBAHXm_PUptEPSXDRswtDsrTYdw5DUsK.js
+++ RollingSky/7mVGnDNzxBAHXm_PUptEPSXDRswtDsrTYdw5DUsK.js
@@ -5,6 +5,10 @@ if (fgnc == 1) {
    $('body').css({'background-image':'url(https://figseu-technology.github.io/FeXBrowser/RollingSky/RSBG/RSBG_2.png)'});
    $('#level-name').html('<img src="STT/STT_1.png" width="96%">');
    $('#level-view').html('<img src="RAA/RAA_1.png" width="92%">');
+} else if (fgnc == 12) {
+   $('body').css({'background-image':'url(https://figseu-technology.github.io/FeXBrowser/RollingSky/RSBG/RSBG_1.png)'});
+   $('#level-name').html('<img src="STT/STT_12.png" width="96%">');
+   $('#level-view').html('<img src="RAA/RAA_12.png" width="92%">');
 } else if (fgnc == 443) {
    $('body').css({'background-image':'url(https://figseu-technology.github.io/FeXBrowser/RollingSky/RSBG/RSBG_443.png)'});
    $('#level-name').html('<img src="STT/STT_443.png" width="96%">');
@@ -14,6 +18,8 @@ if (fgnc == 1) {
 function BGM() {
    if (fgnc == 1) {
 	$('#st_1').get(0).play();
+   } else if (fgnc == 12) {
+	$('#st_12').get(0).play();
    } else if (fgnc == 443) {
 	$('#st_443').get(0).play();
    }
