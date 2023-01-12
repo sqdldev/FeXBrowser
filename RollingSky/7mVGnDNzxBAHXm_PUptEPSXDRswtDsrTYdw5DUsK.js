be1f6d2d343e9d37a0bfa9eba9c69d046ae12472
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2023-01-12 15:20:02 +0900

    Update 7mVGnDNzxBAHXm_PUptEPSXDRswtDsrTYdw5DUsK.js

--- RollingSky/7mVGnDNzxBAHXm_PUptEPSXDRswtDsrTYdw5DUsK.js
+++ RollingSky/7mVGnDNzxBAHXm_PUptEPSXDRswtDsrTYdw5DUsK.js
@@ -2,5 +2,5 @@ let url = location.href;
 let fgnc = url.substr( 72 );
 
 if (fgnc == 1) {
-   $('body').css('background-image', 'url="https://figseu-technology.github.io/FeXBrowser/RollingSky/RSBG/RSBG_2.png"');
+   $('body').css('background-image', 'url(https://figseu-technology.github.io/FeXBrowser/RollingSky/RSBG/RSBG_2.png)');
 }
